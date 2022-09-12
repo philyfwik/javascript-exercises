61df45a69db60f3eb5af72cfbbd98e1da1a93f49
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-12 16:02:19 -0600

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = '';
 
+    if (num < 0) 
+        finalString = 'ERROR';
+    else {
+        for (let i=0; i<num; i++) {
+            finalString += string;
+        }
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
