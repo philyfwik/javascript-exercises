1397d8c38e10addb6a8b1817d48802ddd209a6b0
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-12 16:16:13 -0600

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = '';
 
+    for (let i=1; i<=string.length; i++) {
+        newString += string.charAt(string.length-i);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
