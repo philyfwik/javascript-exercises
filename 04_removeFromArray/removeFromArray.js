0da7c80d02a9452c281e7449294a95d3578a4d93
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-13 16:36:07 -0600

    exercise 4 alternate complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
 const removeFromArray = function(array, ...argument) {
+    /*
     let arr2 = [];
     for (let i=0; i<array.length; i++) {
         if (!argument.includes(array[i]))
             arr2.push(array[i]);
     }
     return arr2;
+    */
+   let arr2 = array.filter(i => !argument.includes(i));
+   return arr2;
 };
 
 console.log(removeFromArray([1,2,3,4], 3, 2));
