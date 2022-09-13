09248b9c7feedfc52de34ed9c50a0c7937e16ab2
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-13 16:27:26 -0600

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argument) {
+    let arr2 = [];
+    for (let i=0; i<array.length; i++) {
+        if (!argument.includes(array[i]))
+            arr2.push(array[i]);
+    }
+    return arr2;
 };
 
+console.log(removeFromArray([1,2,3,4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
