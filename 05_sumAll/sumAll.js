f105762aef3e89deec8de31050c9a133b9b7ec81
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-14 13:54:33 -0600

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let answer = 0;
 
+    if (int1 < 0 || int2 < 0 || typeof(int1) != "number" || typeof(int2) != "number") {
+        answer = 'ERROR';
+    } else if (int1 <= int2) {
+        for (let i=int1; i<=int2; i++)
+            answer += i;
+    } else {
+        for (let i=int2; i<=int1; i++)
+            answer += i;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
