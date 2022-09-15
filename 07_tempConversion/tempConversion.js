0b10b6919479faf2a7c30cc6e6e774b35a19f06b
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-15 12:02:51 -0600

    exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
+  let cels = 0;
 
+  // formula: c = (f - 32) / 1.8
+  cels = (fahr - 32) / 1.8;
+
+  if (cels % 1 === 0)
+    return cels;
+  else
+    return Number(cels.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(cels) {
+  let fahr = 0;
 
+  // formula: f = (c * 1.8) + 32
+  fahr = (cels * 1.8) + 32;
+
+  if (fahr % 1 === 0)
+    return fahr;
+  else
+    return Number(fahr.toFixed(1));
 };
 
 // Do not edit below this line
